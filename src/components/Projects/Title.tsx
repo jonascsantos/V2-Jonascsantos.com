f186716b29feecf34c4c97e5bf3c9d856bc1fb66
Jonas dos Santos <jonas.santos@freshconstruct.com>
2024-03-26 19:39:02 +0100

    Add new project

--- src/components/Projects/Title.tsx
+++ src/components/Projects/Title.tsx
@@ -3,7 +3,7 @@ export function Title() {
     <div className="relative flex w-full p-2  items-center">
       <div className="w-5 border-t border-cloudy-black"></div>
       <h1 className="my-3 px-3 text-2xl font-bold text-cloudy-black flex-shrink">
-        Some Projects I’ve Done
+        Some Projects I’ve Worked On
       </h1>
       <div className="flex-grow max-w-[20vw] border-t border-cloudy-black"></div>
     </div>
