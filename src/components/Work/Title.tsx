881ebd48934ca9da3349497aa0b93e76f7880ca8
Jonas dos Santos <jonas.santos@freshconstruct.com>
2024-04-09 14:51:34 -0300

    Title Changes and alignment

--- src/components/Work/Title.tsx
+++ src/components/Work/Title.tsx
@@ -2,7 +2,7 @@ export function Title() {
   return (
     <div className="relative flex w-full px-2 items-center">
       <div className="w-5 border-t border-cloudy-white"></div>
-      <h1 className="my-3 px-3 text-2xl font-bold text-cloudy-white flex-shrink">
+      <h1 className="my-3 px-3 text-2xl sm:text-3xl md:text-4xl  font-bold text-cloudy-white flex-shrink">
         Where I've worked 💼
       </h1>
       <div className="flex-grow max-w-[20vw] border-t border-cloudy-white"></div>
