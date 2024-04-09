3e0f2d074fd76c3afde427f05a27142b3ceb855c
jonascsantos <jonas.cassiano@hotmail.com>
2024-04-09 14:51:34 -0300

    Title Changes and alignment

--- src/components/Contact/Title.tsx
+++ src/components/Contact/Title.tsx
@@ -2,7 +2,7 @@ export function Title() {
   return (
     <div className="relative flex w-full p-2  items-center">
       <div className="w-5 border-t border-cloudy-black"></div>
-      <h1 className="my-3 px-3 text-2xl font-bold text-cloudy-black flex-shrink">
+      <h1 className="my-3 px-3 text-2xl sm:text-3xl md:text-4xl font-bold text-cloudy-black flex-shrink">
         Let's Connect! 😄
       </h1>
       <div className="flex-grow max-w-[20vw] border-t border-cloudy-black"></div>
--- src/components/Projects/Title.tsx
+++ src/components/Projects/Title.tsx
@@ -1,8 +1,8 @@
 export function Title() {
   return (
-    <div className="relative flex w-full p-2  items-center">
+    <div className="relative flex w-full px-2  items-center">
       <div className="w-5 border-t border-cloudy-black"></div>
-      <h1 className="my-3 px-3 text-2xl font-bold text-cloudy-black flex-shrink">
+      <h1 className="my-3 px-3 text-2xl sm:text-3xl md:text-4xl font-bold text-cloudy-black flex-shrink">
         Some Projects I’ve Worked On 👨‍💻
       </h1>
       <div className="flex-grow max-w-[20vw] border-t border-cloudy-black"></div>
