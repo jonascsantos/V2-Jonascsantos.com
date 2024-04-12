deedfb247d170e8459d75d826bfdfac0099eca48
Jonas dos Santos <jonas.santos@freshconstruct.com>
2024-04-11 22:57:34 -0300

    feature: Social links

--- src/components/Hero/Social.tsx
+++ src/components/Hero/Social.tsx
@@ -1,4 +1,5 @@
 import { Github, Linkedin, Instagram, Mail } from "lucide-react";
+import Link from "next/link";
 
 interface SocialProps {
   leftAligned?: boolean
@@ -8,16 +9,22 @@ export const Social = ({ leftAligned }: SocialProps) => {
   return (
     <div className={`mt-5 flex w-full gap-20 justify-around md:justify-start text-primary`}>
       <div className="w-10 h-10 flex items-center justify-center rounded-lg shadow-button">
-        <Github size={30} />
+        <Link target={"_blank"} href={"https://github.com/jonascsantos"}>
+          <Github size={30} />
+        </Link>
       </div>
       <div className="w-10 h-10 flex items-center justify-center rounded-lg shadow-button">
-        <Linkedin size={30} />
+        <Link target={"_blank"} href={"https://www.linkedin.com/in/jonascsantos"}>
+          <Linkedin size={30} />
+        </Link>
       </div>
       {/* <div className="w-10 h-10 flex items-center justify-center rounded-lg shadow-button">
         <Instagram size={30} />
       </div> */}
       <div className="w-10 h-10 flex items-center justify-center rounded-lg shadow-button">
-        <Mail size={30} />
+        <Link target={"_blank"} href={"mailto:contact@jonascsantos.com"}>
+          <Mail size={30} />
+        </Link>
       </div>
     </div>
   );
