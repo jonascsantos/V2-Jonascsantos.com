e2905700719b065bfebcc203a073c01c0cb8e6b2
jonascsantos <jonas.cassiano@hotmail.com>
2024-04-23 15:28:46 -0300

    OG image and favicon changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <head />
+      <head>
+        <meta
+          property="og:image"
+          content="https://jonascsantos.com/ogTagImage.png"
+        />
+        <meta property="og:image:width" content="500" />
+        <meta property="og:image:height" content="500" />
+        <meta property="og:url" content="https://www.jonascsantos.com" />
+        <meta property="og:site_name" content="jonascsantos" />
+        <meta property="og:type" content="website" />
+        <meta
+          name="keywords"
+          content="Jonascsantos, Jonas Cassiano, Jonas dos Santos, jonascsantos.com, Developer, React, React Native, React.JS"
+        />
+      </head>
       <body className="bg-[#F8F9FF] text-[#324B50]">
         <h1 className={"font-bold text-3xl"}></h1>
         {children}
